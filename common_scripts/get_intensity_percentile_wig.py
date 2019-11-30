df035eb19c01f99e133a950a950a3751d1767e97
Tara Eicher <eicher.33@osu.edu>
2019-11-30 12:39:00 -0500

    Added a script for getting just the intensity percentile from the WIG file

---
+++ common_scripts/get_intensity_percentile_wig.py
@@ -0,0 +1,17 @@
+import wig_and_signal_utils as wsu
+import sys
+def main():
+    # Input
+    wig_file = open(sys.argv[1], "r")
+    resolution = int(sys.argv[2])
+    region_size = float(sys.argv[3])
+    percentile = float(sys.argv[4])
+    percentile_cutoff_file = open(sys.argv[5], "w")
+    
+    # Percentile
+    wsu.get_intensity_percentile(percentile, wig_file, resolution)
+    percentile_cutoff_file.write(str(percentile_cutoff))
+    percentile_cutoff_file.close()
+    
+if __name__ == "__main__":
+    main()
